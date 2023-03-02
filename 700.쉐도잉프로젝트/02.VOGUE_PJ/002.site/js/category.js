92d87c173b10be1374136c78c95411a7a09b6a17
simda96 <sim5995@naver.com>
2023-03-02 15:43:18 +0900

    123

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/category.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/category.js
@@ -47,9 +47,60 @@ function loadFn(){
     stit.innerText = mdata["제목"];
 
     // (2) LNB 메뉴 넣기
+    // 대상 : .lnb -> lnb변수
+    // 코드넣기 :
+    // <ul><li><a href="#">메뉴</li></ul>
+    // mdata["메뉴"] 값이 "없음"이면 lnb를 지우고
+    // 아니면 배열값으로 메뉴가 있으므로 배열만큼
+    // 위의 ul>li>a 코드를 넣어 준다!
 
-    // (3) 컨텐츠 박스에 pm과 같은 이름의 class넣기
-    cont.classList.add(pm.replace(" & ","-"));
+    // 메뉴값 담기
+    let mvalue = mdata["메뉴"];
+
+    if(mvalue==="없음"){
+        // LNB박스 제거!
+        lnb.remove();
+    } ////// if /////////////////
+    else{  // 메뉴가 배열에 있음!
+        let temp = "<ul>"; // 임시변수
+
+        // 메뉴 배열만큼 돌아서 코드생성
+        mvalue.forEach((val)=>{ // val 배열값!
+            temp += `
+                <li>
+                    <a href="#">${val}</a>
+                </li>
+            `;
+        }); ////// forEach ////////
+
+        temp += "</ul>";
+
+        // LNB박스에 html넣기!
+        lnb.innerHTML = temp;
+
+    } ////////////// else /////////////
+    
+
+
+    // (3) 내용 타이틀 넣기 : 대상 - contit변수
+    // -> h2개수만큼 순번대로 mdata["타이틀"][순번]
+    // h2를 돌릴 떄 for of말고 forEach() 메서드로 사용!
+    // forEach((요소,순번)=>{코드})
+    contit.forEach((ele,idx)=>{
+        ele.innerHTML = mdata["타이틀"][idx]
+    }); //////// forEach ///////
+
+
+
+    // (4) 컨텐츠 박스에 pm과 같은 이름의 class넣기
+    cont.classList.add(mdata["경로"]);
+    // "경로"속성의 값이 실제 클래스명과 일치함!
+    // cont.classList.add(pm.replace(" & ","-"));
     // replace(바뀔값,바꿀값)
+
+    // (5) 탭메뉴 출력 title요소 데이터 넣기
+    // 기존값을 앞에 "제목"속성값을 넣어준다!
+    // 대상 : title요소 -> titag 변수
+    titag.innerText = mdata["제목"] + titag.innerText;
 
 } //////////////// loadFn 함수 ///////////////////
