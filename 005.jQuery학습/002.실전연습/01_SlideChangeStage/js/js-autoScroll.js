4853f8b22980402ee077893309590d5ae33ffdda
simda96 <sim5995@naver.com>
2023-03-06 15:41:44 +0900

    123

---
+++ 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/js-autoScroll.js
@@ -0,0 +1,12 @@
+// JS로 구현한 자동페이지 휠 JS : js-autoScroll.js
+
+// 로딩함수 호출 ///////////
+window.addEventListener("DOMContentLoaded",loadFn);
+
+function loadFn(){
+
+    // 호출확인
+    console.log("로딩완료!");
+    
+
+} /////////////////// loadFn 함수 /////////////
