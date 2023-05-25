f177fca7b461de602edd478964a33ee1d6d02f64
simda96 <sim5995@naver.com>
2023-05-25 17:17:34 +0900

    123

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-router.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-router.js
@@ -6,7 +6,7 @@
 let Glist = {
     template: `
         <section>
-            <!-- 필터옵션 체크박스구역 --> 
+            <!-- 필터옵션 체크박스구역 -->
             <div id="optbx">
                 <label for="men">남성</label>
                 <input type="checkbox" id="men"
@@ -319,10 +319,12 @@ const Detail = {
                     <div>
                         <!--버튼영역-->
                         <button class="btn btn1">BUY NOW</button>
-                        <button class="btn scbtn" @click="$store.commit('setData', $route.params.id-1)">
-                            SHOPPING CART
-                        </button>
-                        <button class="btn">WISH LIST</button>
+                        <button class="btn scbtn"
+                        @click="$store.commit('setData',$route.params.id-1)"
+                        >SHOPPING CART</button>
+                        <button class="btn"
+                        @click="$store.commit('clearData')"
+                        >CLEAR CART</button>
 
                     </div>
                 </section>
