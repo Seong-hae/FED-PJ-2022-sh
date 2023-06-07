78853be094c1d3e3b01399277d577a11e344c4d0
simda96 <sim5995@naver.com>
2023-06-07 15:48:30 +0900

    123

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Main.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Main.js
@@ -1,15 +1,20 @@
 // DC 메인 페이지 컴포넌트
 import React from "react";
-import Ban from "./Ban";
-import MenuBtn from "./MenuBtn";
+import Ban from "./modules/Ban";
+import MenuBtn from "./modules/MenuBtn";
+import VidIntro from "./modules/VidIntro";
 
 const Main = () => {
     return(
         <>
+            {/* 1. 배너모듈 */}
             <Ban cat="main"/>
+            {/* 2. 메뉴버튼모듈 */}
             <MenuBtn />
+            {/* 3. 비디오소개 모듈 */}
+            <VidIntro pg="main" mm=""/>
         </>
     );
-}; /////////////////// Characters /////////////////
+}; /////////////////// Main /////////////////
 
 export default Main;
