145869aa7ba39eb36786de0f0ca30212c874cd97
Seong-hae <ayay1123@naver.com>
2023-06-13 23:03:13 +0900

    123

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app 22/src/dc/Video.js
@@ -0,0 +1,14 @@
+// DC Video 페이지 컴포넌트
+import React from "react";
+import isrc from "./ImgSrc";
+import VidIntro from "./modules/VidIntro";
+
+const Video = () => {
+    return(
+        <>  
+            <VidIntro pg="VIDEO" mm="on" />
+        </>
+    );
+}; /////////////////// Video /////////////////
+
+export default Video;
