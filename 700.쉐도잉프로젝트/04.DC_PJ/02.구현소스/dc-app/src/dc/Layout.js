a12e361085d90514c24a8bc58d40bf58930cb674
simda96 <sim5995@naver.com>
2023-07-03 17:20:23 +0900

    123

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Layout.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Layout.js
@@ -1,197 +1,240 @@
 // 메인 레이아웃 컴포넌트
+
 import Logo from "./Logo";
 import "./css/layout.css";
+import $ from 'jquery';
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { gnb_data, bmenu } from "./data/common";
-
-/* 폰트어썸 임포트 */
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 import { useState } from "react";
 import ScrollTop from "./common/ScrollTop";
-/******************************************************* 
+
+// 폰트어섬 임포트
+import { faCamera, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+/***********************************************************************************
     [ 리액트 라우터와 연결하여 사용되는 라우터 컴포넌트 ]
-    1. <Link to="/경로명"></Link>
-    -> to속성의 경로는 <Route path="/경로명"> 과 일치함!
+1.<Link to ="/경로명"></Link>
+-to 속성의 경로는 <Route path="/경로명">과 일치함
 
-    2. <Outlet />
-    -> 라우터 연결 컴포넌트 출력자리 컴포넌트
-*******************************************************/
+2.<Outlet />
+-라우터 연결 컴포넌트가 출력되는 자리 컴포넌트를 말함
+***********************************************************************************/
 
-const Layout = () => {
+const Layout = ()=>{
+    
+    // 자식 컴포넌트 값 전달 테스트 함수
+    const callMe = (x)=>{
+        console.log("누구? : ", x);
+    }; ///////////////// callMe ////////////////////
 
-    // 라우터 이동메서드 셋팅!
-    const goNav = useNavigate();   
 
+    // 라우터를 이용하여 데이터 보내기 : 라우터 이동메서트 세팅하기
+    const goNav = useNavigate();
+    
 
-    // 자식컴포넌트 값 전달 테스트 함수
-    const callMe = (x) => {
-        console.log("누구?", x);
-    }; ////////// callMe /////////////
-
-    // 로그인 상태  Hook변수 : 로컬쓰 "minfo" 초기할당!
+    // 로그인 상태 Hook변수 : 로컬스토리지의 "minfo" 항목의 값을 초기 할당함
     const [logSts, setLogSts] = useState(localStorage.getItem("minfo"));
-    // 로그인 환영메시지 Hook변수
+
+    // 로그인 환영 메시지 Hook변수
+    // : 메시지도 상태관리해줘야 그때그때 다르게 들어간다고함
+    // : 리액트만 사용해서 구현하면 알아서 타이밍 맞추는데, 돔 건드리는 코딩(제이쿼리같은거)을 같이 한다면 타이밍 맞추는데 힘들수도 있음
     const [logMsg, setLogMsg] = useState("");
-    // 로그인 환영메시지 스타일
+
+    // 로그인 환영 메시지 스타일
     const logstyle = {
         position: "absolute",
         left: "50%",
-        transform: "translateX(-50%)",
-    }; //////// logstyle ///////////
+        transform: "translateX(-50%)"
+        
+    }; //////////// logstyle ////////////////
 
-    // 로그인 셋팅 함수 //////////
-    // -> ScrollTop.js 의 useEffect 함수구역에서 호출!
-    const setLogin = () => {
-        // 1. 로그인 Hook변수 업데이트하기
+    // [ 로그인 세팅 함수 ] 👉 ScrollTop.js의 useEffect 함수 구역에서 호출함
+    const setLogin = ()=>{
+        // 1.로그인 Hook변수 업데이트하기
         setLogSts(localStorage.getItem("minfo"));
 
-        // 2. 로컬쓰값이 null이 아니면 메시지 뿌리기
-        if (localStorage.getItem("minfo")) {
-            // 메시지 셋팅하기 : 객체안의 "unm"속성이 사용자이름!
-            setLogMsg("Welcome " + JSON.parse(localStorage.getItem("minfo"))["unm"]);
-        } ////////////// if ///////////////
-    }; ///////// setLogin ////////////
+        // 2.만약 로컬스토리지의 값이 null이 아니면 메시지 뿌리기
+        if(localStorage.getItem("minfo")){
+            // null이면 false처리 나니까, 널이 아니면 중괄호 영역에 들어올 수 있음
+            
+            // 메시지 세팅하기 : 객체 안의 "unm"속성이 사용자 이름!
+            setLogMsg("😎 Welcome " + JSON.parse(localStorage.getItem("minfo"))["unm"]);
+        }
+    }; //////////////// setLogin //////////////////
 
-    // 로그아웃 함수 ///////////////////
-    // -> LOGOUT 버튼에서 호출함!
-    const logout = () => {
-        // 1. 로컬쓰 "minfo" 삭제하기
+
+    // [ 로그아웃 함수 ] 👉 LOGOUT 버튼에서 호출함
+    const logout = ()=>{
+        // 1. 로컬스토리지의 "minfo" 항목만 삭제하기 (clear하면 모든 항목이 다 지워지니 주의할 것)
         localStorage.removeItem("minfo");
-        // 2. 로그인상태 Hook 변수 업데이트하기
-        setLogSts(null);
+
+        // 2. 로그인 상태 Hook 변수 업데이트하기
+        setLogSts(null); // 비어있다는 의미로 null 주는 것임! (null도 데이터의 하나임!)
         console.log("로그아웃됨!", logSts);
-    }; ////////////// logout ///////////
 
-    // 검색창 보이기 함수 ///////
-    const showSearch = (e) => {
-        // 1. 자기자신 숨기기
-        e.target.style.opacity = "0";
+        
+    }; ////////////// logout ////////////////
 
-        // 2. 검색창 보이기
-        let tg =
-        document.querySelector(".searchingGnb");
+
+
+    // [ 검색창 보이기 함수 ] ///////////////////
+    const showSearch = ()=>{
+        // 1. a요소 숨기기
+        document.querySelector(".searchingGnb+a").style.opacity = "0";
+
+        // 2. 검색창 보이기 + 포커스 넣기
+        let tg = document.querySelector(".searchingGnb");
         tg.style.display = "block";
         tg.querySelector("input").focus();
 
-    }; ////////// showSearch /////////////
 
-    // 입력창에서 엔터키를 누르면 검색함수 호출!
-    const enterKey = (e) => {
-        if (e.key === "Enter") goSearch();
-    }; //////////// enterKey 함수 ////////////
+    }; ////////////// showSearch 함수 //////////////////
 
-    // 검색페이지로 검색어와 함께 이동하기 //////
-    const goSearch = () => {
+
+    // [ 입력창에서 엔터키를 누르면 검색 함수 호출하기 ] //////////////////////
+    const enterKey = (e)=>{
+        // JS 오리지널 문법에서 엔터키는 13번?이었음
+
+        // 이벤트의 key가 엔터키일 때 검색 함수(schList) 호출하도록 if문으로 제어하기
+        if(e.key === 'Enter') goSearch();
+    }; //////////////// enterKey 함수 /////////////////////
+
+
+
+    // [ 검색 페이지로 검색어와 함께 이동하기 ] /////////////////////////////
+    const goSearch = ()=>{
         // 검색어 읽어오기
         let kw = document.querySelector(".searchingGnb input").value;
-        console.log("검색어:",kw);
-        // 라우터 이동하기 : 전달값 가져가기(검색어)
-        goNav('/res',{state:{keyword:kw}})
-    }; ///////////// goSearch ////////////////
+        console.log("검색어 : ", kw);
 
-    return (
+        // 라우터 이동하기 : 전달값(검색어) 가져가기
+        goNav('/res',{state:{keyword:kw}});
+
+    }; //////////////////// goSearch 함수 //////////////////////
+
+
+    // 햄버거 버튼 클릭시 메뉴 등장하기 ///////////////
+    const chgMenu = ()=>{
+        // let topA = $(".top");
+        // // 제이쿼리 is 메서드! : 있는지 없는지 확인 후 t/f 리턴
+        // let isOn = topA.is(".on");
+
+        // // on클래스가 있으면 제거하기
+        // if(isOn) topA.removeClass("on");
+        // // 없으면 추가하기
+        // else topA.addClass("on");
+
+        // 토글 클레스 메서드로 on 클래스를 추가/제거하기
+        $(".top").toggleClass("on");
+
+    }; ///////////////// chgMenu 함수 /////////////////
+
+    // 메뉴 클릭시 닫기 부가 기능 함수
+    const rmCls = ()=>$(".top").removeClass("on");
+
+
+
+    return(
         <>
-            {/* 라우터 갱신될때 스크롤 상단이동 모듈작동함!
-            + 로그인셋팅함수 호출전달하기! 자식에게 setLogin함수전달 */}
-            <ScrollTop sfn={setLogin} />
+            <ScrollTop sfn={setLogin} /> 
+            {/* 👆라우터 갱신될 때 스크롤 상단 이동 모듈 작동함! + 로그인 세팅 함수 호출 전달하기 (자식에게 setLogin함수 전달) : 세팅 위치는 상관 없음! 위쪽이든 아래쪽이든 간에 <BrowserRouter>안에 있으면 됨 */}
             {/* 1.상단영역 */}
             <header className="top">
-                {/* 로그인 환영메시지 : 조건-logSts값이 null이 아니면 */}
-                {logSts !== null && (
+
+                {/* 로그인 환영 메시지 : 조건 - logSts값이 null이 아니면 */}
+                {
+                    logSts !== null &&
                     <div className="logmsg" style={logstyle}>
                         {logMsg}
                     </div>
-                )}
+                }
 
-                {/* 네비게이션 파트 */}
+
+                {/* 내비게이션 파트 */}
                 <nav className="gnb">
                     <ul>
                         <li>
-                            <Link to="/main">
-                                <Logo gb="top" tt={callMe} />
-                            </Link>
+                            <Link to='/main'><Logo gb="top" tt={callMe} /></Link>
                         </li>
-                        {gnb_data.map((v, i) => (
-                            <li key={i}>
-                                <Link to={v.link}>{v.txt}</Link>
-                                {/* {console.log(v.sub)} */}
-                                {/* v.sub가 없으면 undefined */}
-                                {
-                                    // 조건식 && 출력코드
-                                    // 조건: sub데이터가 없지 않으면
-                                    // undefined - 정의되지 않은값
-                                    // null - 빈값
-                                    // 위의 두가지는 데이터가 없다는 값임!
-                                    v.sub != undefined && (
+                        {
+                            gnb_data.map((v, i)=>
+                            // Link 컴포넌트에 다른 이벤트 걸면 에러 발생함! 그러니까 위의 li에 이벤트를 거는 것임! 이래도 가능함! (이벤트 버블링 때문!)
+                            // chgMenu는 토글로 클래스 추가/제거 기능만 있으니까 그냥 이 함수 씀~
+                                <li key={i} onClick={rmCls}>
+                                    <Link to={v.link}>{v.txt}</Link>
+                                    {/* console.log(v.sub) */}
+                                    {/* v.sub가 없으면 undefined! */}
+                                    {
+                                        // 조건식 && 출력코드
+                                        // 조건 : 서브데이터(배열)가 없지 않으면 (=있으면) 출력!
+                                        // undefined - 정의되지 않은 값
+                                        // null - 빈 값
+                                        // 👉위의 두가지는 데이터가 없다는 값!
+                                        v.sub != undefined &&
                                         <div className="smenu">
                                             <ol>
-                                                {v.sub.map((v, i) => (
+                                                {
+                                                    v.sub.map((v, i)=>
                                                     <li key={i}>
                                                         <Link to={v.link}>{v.txt}</Link>
                                                     </li>
-                                                ))}
+                                                    )
+                                                }
                                             </ol>
                                         </div>
-                                    )
-                                }
-                            </li>
-                        ))}
-
-                        <li style={{ marginLeft: "auto" }}>
-                            {/* 검색입력박스 */}
-                            <div className="searchingGnb">
+                                    }
+                                </li>
+                            )
+                        }
+                        {/* 마진레프트 : 오토! -> 자동으로 끝으로 가게 함(?) */}
+                        <li style={{marginLeft:"auto"}}>
+                            {/* 검색 입력 박스 */}
+                            {/* 검색박스 */}
+                            <div className='searchingGnb'>
                                 {/* 검색버튼 돋보기아이콘 */}
-                                <FontAwesomeIcon
-                                    icon={faSearch}
-                                    className="schbtnGnb"
-                                    title="Open search"
-                                    onClick={goSearch}
+                                <FontAwesomeIcon 
+                                icon={faSearch}
+                                className='schbtnGnb'
+                                title='Open search'
+                                onClick={goSearch}
                                 />
                                 {/* 입력창 */}
-                                <input
-                                    id="schinGnb"
-                                    type="text"
-                                    placeholder="Filter by Keyword"
-                                    onKeyUp={enterKey}
-                                />
+                                <input id='schinGnb' type='text' 
+                                placeholder='Filter by Keyword' onKeyUp={enterKey} />
                             </div>
-                            {/* 검색기능링크 - 클릭시 검색창보이기! */}
-                            <a href="#" onClick={showSearch}>
+                            {/* 검색기능 링크 : 클릭시 검색창 보이기! */}
+                            <a href="#" onClick={showSearch} >
                                 <FontAwesomeIcon icon={faSearch} />
                             </a>
                         </li>
                         {
-                            /* 회원가입,로그인은 로그인아닌 상태일때만 */
-                            logSts === null && (
-                                <>
-                                    <li>
-                                        <Link to="/mem">Join Us</Link>
-                                    </li>
-                                    <li>
-                                        <Link to="/login">LOGIN</Link>
-                                    </li>
-                                </>
-                            )
-                        }
-
-                        {
-                            /* 로그아웃버튼은 로인인상태일때만 */
-                            logSts !== null && (
+                            /* 회원가입, 로그인은 로그인하지 않은 상태일때만 보이게 하기 */
+                            logSts === null &&
+                            <>
                                 <li>
-                                    <a href="#" onClick={logout}>
-                                        LOGOUT
-                                    </a>
+                                    <Link to="/mem" onClick={rmCls}>Join Us</Link>
                                 </li>
-                            )
+                                <li>
+                                    <Link to="/login" onClick={rmCls}>LOG IN</Link>
+                                </li>
+                            </>
+                        }
+                        {
+                            /* 로그아웃버튼은 로그인 상태일때만 보이게 하기 */
+                            logSts !== null &&
+                            <li onClick={rmCls}>
+                                <a href="#" onClick={logout}>LOG OUT</a>
+                            </li>
                         }
                     </ul>
+                    {/* 햄버거 버튼 */}
+                    <button className="hambtn" onClick={chgMenu}></button>
                 </nav>
             </header>
-            {/* 2. 메인영역 */}
+            {/* 2.메인영역 */}
             <main className="cont">
-                {/* 출력파트 : 각 페이지의 컴포넌트가 출력됨 */}
+                {/* 출력 파트 : 각 페이지의 컴포넌트가 출력되는 부분 */}
                 <Outlet />
             </main>
             {/* 3.하단영역 */}
@@ -207,6 +250,7 @@ const Layout = () => {
                                     <a href={v.link} target="_blank">
                                         {v.txt.toUpperCase()}
                                     </a>
+                                    {i!=4?" |":""}
                                 </li>
                             ))}
                         </ol>
@@ -216,7 +260,7 @@ const Layout = () => {
             </footer>
         </>
     );
-}; ////////// Layout 컴포넌트 ///////
+}; /////////////// Layout 컴포넌트 ////////////////
 
 // 내보내기
 export default Layout;
